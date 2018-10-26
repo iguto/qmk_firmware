8e0064ba59ba98cf5c8023ba3dd6f5609a23011a
iguto <clc.iguto@gmail.com>
2018-10-26 21:34:11 +0900

    set ime on/off on F23/F24 on lower/raise tap

--- keyboards/fortitude60/keymaps/iguto/keymap.c
+++ keyboards/fortitude60/keymaps/iguto/keymap.c
@@ -36,6 +36,8 @@ enum custom_keycodes {
 #define IME_OFF LCTL(KC_SPACE)
 #define IME_ON LCTL(LSFT(KC_SPACE))
 #define LC LCTL
+#define LW_IMON LT(_LOWER, KC_F23)
+#define RS_IMOF LT(_RAISE, KC_F24)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -57,7 +59,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,     KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                   KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
   KC_LSHIFT,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                   KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT, \
   KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B, KC_LBRC, KC_RSFT, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, MISC, \
-                 KC_LGUI, KC_LALT,   LOWER,  KC_SPC, KC_LCTL,  KC_ENT, LTE_SP, RAISE,    KC_RALT, KC_RGUI\
+                 KC_LGUI, KC_LALT,  LW_IMON,  KC_SPC, KC_LCTL,  KC_ENT, LTE_SP, RS_IMOF,   KC_RALT, KC_RGUI\
 ),
 
 /* Colemak
