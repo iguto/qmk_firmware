c17fc78ccd7c3eced34897ec707edbb1de7688f7
iguto <clc.iguto@gmail.com>
2020-09-20 13:32:32 +0900

    format keylayout

--- keyboards/lily58/keymaps/iguto/keymap.c
+++ keyboards/lily58/keymaps/iguto/keymap.c
@@ -62,10 +62,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
  [_QWERTY] = LAYOUT( \
-  KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                         KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_GRV, \
-  KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
-  KC_LCTRL, KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT, \
-  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,     TEN_KEY,  KC_RBRC,  KC_N,       KC_M,    KC_COMM, KC_DOT,  KC_SLSH, MISC, \
+  KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_GRV, \
+  KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,\
+  KC_LCTRL, KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT, \
+  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,  TEN_KEY,  KC_RBRC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, MISC, \
                              KC_LGUI, KC_LALT, LOWER, KC_SPC,   KC_ENT,   RAISE,   KC_BSPC, KC_RGUI \
 ),
  [_NAGINATA] = LAYOUT( \
@@ -138,8 +138,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_TEN_KEY] = LAYOUT( \
   _______, KC_BSPC, XXXXXXX, KC_SLSH, S(KC_8), XXXXXXX,                     _______, _______, _______, _______, _______, _______, \
   _______, KC_EQL,  KC_7,    KC_8,    KC_9,    KC_MINS,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, \
-  _______, XXXXXXX,  KC_4,    KC_5,    KC_6,    S(KC_EQL),                   _______, KC_MINS, KC_EQL, KC_LBRC, KC_RBRC,  KC_BSLS, \
-  _______, XXXXXXX,  KC_1,    KC_2,    KC_3,    KC_0,      _______, _______, KC_MINS, KC_QUOT, KC_NUBS, _______, _______, _______,\
+  _______, XXXXXXX,  KC_4,    KC_5,    KC_6,   S(KC_EQL),                   _______, KC_MINS, KC_EQL, KC_LBRC, KC_RBRC,  KC_BSLS, \
+  _______, XXXXXXX,  KC_1,    KC_2,    KC_3,    KC_0,     _______, _______, KC_MINS, KC_QUOT, KC_NUBS, _______, _______, _______,\
                     KC_DOT, _______, _______,  KC_PENT,   _______, _______, _______, _______ \
 ),
 
@@ -298,18 +298,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 	}
 	return false;
 	break;
-    //case EISU:
-    //  if (record->event.pressed) {
-    //    naginata_off();
-    //  }
-    //  return false;
-    //  break;
-    //case KANA2:
-    //  if (record->event.pressed) {
-    //    naginata_on();
-    //  }
-    //  return false;
-    //  break;
   }
   if (!process_naginata(keycode, record)) {
     return false;
