5e1ed55bc8b2b8db3fe582ca656f9dd7156b2d0c
iguto <clc.iguto@gmail.com>
2020-10-12 16:56:01 +0900

    mac ime on/off

--- keyboards/lily58/keymaps/iguto/keymap.c
+++ keyboards/lily58/keymaps/iguto/keymap.c
@@ -19,7 +19,7 @@ extern uint8_t is_master;
 NGKEYS naginata_keys;
 
 #define _QWERTY 0
-#define _NAGINATA 2
+#define _NAGINATA 4
 #define _LOWER 5
 #define _RAISE 10
 #define _TEN_KEY 11
@@ -28,6 +28,7 @@ NGKEYS naginata_keys;
 
 enum custom_keycodes {
   QWERTY = NG_SAFE_RANGE,
+  MAC,
   LOWER,
   RAISE,
   TEN_KEY,
@@ -36,12 +37,15 @@ enum custom_keycodes {
   NAGINATA,
   EISU,
   KANA2,
+  IMEON,
+  IMEOFF,
 };
 
 #define LOWER MO(_LOWER)
 #define RAISE MO(_RAISE)
 #define MISC MO(_MISC)
 #define TEN_KEY TG(_TEN_KEY)
+#define MACCMD CMD_T(KC_SPC)
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -94,7 +98,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                     KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, \
   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,                    KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_TILD, \
   _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, \
-                             _______, _______, _______, _______, KC_F13,  _______, _______, _______\
+                             _______, _______, _______, _______, IMEOFF,  _______, _______, _______\
 ),
 /* RAISE
  * ,-----------------------------------------.                    ,-----------------------------------------.
@@ -116,7 +120,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL, \
   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                       _______, KC_MINS, KC_EQL, KC_LBRC, KC_RBRC,  KC_BSLS, \
   KC_LSFT, KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,   _______, _______,  KC_MINS, KC_QUOT, KC_NUBS, _______, _______, _______,\
-                             _______, _______, _______,  KC_F14,  _______,  _______, _______, _______ \
+                             _______, _______, _______,  IMEON,  _______,  _______, _______, _______ \
 ),
 
 
@@ -184,7 +188,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, RGB_TOG, RGB_HUI, RGB_SAI, RGB_VAI, \
-  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, RGB_MOD, RGB_HUD, RGB_SAD, RGB_VAD,\
+  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, MAC    , RGB_MOD, RGB_HUD, RGB_SAD, RGB_VAD,\
                              _______, _______, _______, _______, _______,  _______, _______, _______ \
   )
 };
@@ -245,6 +249,8 @@ void oled_task_user(void) {
 }
 #endif//SSD1306OLED
 
+bool is_mac = false;
+
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   if (record->event.pressed) {
 #ifdef OLED_DRIVER_ENABLE
@@ -289,15 +295,53 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         return false;
         break;
     case NAGINATA:
-	if (record->event.pressed) {
-	  if (naginata_state()) {
-	    naginata_off();
-	  } else {
-	    naginata_on();
-	  }
-	}
-	return false;
-	break;
+      if (record->event.pressed) {
+        if (naginata_state()) {
+          naginata_off();
+        } else {
+          naginata_on();
+        }
+      }
+      return false;
+      break;
+    case MAC:
+      if (record->event.pressed) {
+        is_mac = !is_mac;
+      }
+      return false;
+      break;
+    case IMEON:
+      if (record->event.pressed) {
+        if (is_mac) {
+          register_code(KC_LANG1);
+        } else {
+          register_code(KC_F14);
+        }
+      } else {
+        if (is_mac) {
+          unregister_code(KC_LANG1);
+        } else {
+          unregister_code(KC_F14);
+        }
+      }
+      return false;
+      break;
+    case IMEOFF:
+      if (record->event.pressed) {
+        if (is_mac) {
+          register_code(KC_LANG2);
+        } else {
+          register_code(KC_F13);
+        }
+      } else {
+        if (is_mac) {
+          unregister_code(KC_LANG2);
+        } else {
+          unregister_code(KC_F13);
+        }
+      }
+      return false;
+      break;
   }
   if (!process_naginata(keycode, record)) {
     return false;
