72a1ddd232a61b4aff4c5232e7da5bc795177487
iguto <clc.iguto@gmail.com>
2021-05-07 17:03:30 +0900

    add working new naginata

--- keyboards/claw44/keymaps/iguto/keymap.c
+++ keyboards/claw44/keymaps/iguto/keymap.c
@@ -70,7 +70,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______,NG_Q   ,NG_W   ,NG_E   ,NG_R   ,NG_T   ,                NG_Y   ,NG_U   ,NG_I   ,NG_O   ,NG_P   ,_______, \
     _______,NG_A   ,NG_S   ,NG_D   ,NG_F   ,NG_G   ,                NG_H   ,NG_J   ,NG_K   ,NG_L   ,NG_SCLN,_______, \
     _______,NG_Z   ,NG_X   ,NG_C   ,NG_V   ,NG_B   ,                NG_N   ,NG_M   ,NG_COMM,NG_DOT ,NG_SLSH,_______, \
-                    _______,_______,NG_SHFT,_______,                _______,NG_SHFT,_______,_______
+                    _______,NG_SHFT, LOWER, _______,              _______,RAISE,NG_SHFT, EMACS
   ),
 
     [_RAISE] = LAYOUT( \
@@ -156,6 +156,11 @@ void render_layer_state(void) {
             break;
         case _ADJUST:
             oled_write_ln_P(PSTR("Layer: Adjust"), false);
+        case _MISC:
+            oled_write_ln_P(PSTR("Layer: Misc"), false);
+            break;
+        case _NAGINATA:
+            oled_write_ln_P(PSTR("Layer: NAGINATA"), false);
             break;
         default:
             oled_write_ln_P(PSTR("Layer: Undefined"), false);
